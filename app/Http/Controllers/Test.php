ccc3d6870cf3cce529d87b602498806712c6beda
qaweeds <qaweeds@gmail.com>
2021-06-24 12:12:46 +0300

    изменилась логика расчера сумм по оптовым магазинам

--- app/Http/Controllers/Test.php
+++ app/Http/Controllers/Test.php
@@ -13,8 +13,8 @@ use Carbon\Carbon;
 
 class Test extends Controller
 {
-public function __invoke()
-{
-    // TODO: Implement __invoke() method.
-}
+    public function __invoke()
+    {
+        // TODO: Implement __invoke() method.
+    }
 }
