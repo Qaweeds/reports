bca4934b699281fd6c846166e4256530728e87e5
qaweeds <qaweeds@gmail.com>
2021-08-16 09:42:49 +0300

    qq

--- app/Http/Controllers/Hats/HatsController.php
+++ app/Http/Controllers/Hats/HatsController.php
@@ -10,52 +10,16 @@ use Illuminate\Support\Facades\DB;
 class HatsController extends BaseHatsController
 {
     protected $date;
+    protected $stores;
 
     public function __construct()
     {
 //        dd(request()->all());
         $date = request()->get('period');
-        if(isset($date) and $date < Carbon::now()->format('Y-m-d')) $this->date = Carbon::parse($date);
+        if (isset($date) and $date < Carbon::now()->format('Y-m-d')) $this->date = Carbon::parse($date);
         else $this->date = Carbon::now()->subDay();
 
     }
-
-    public function store()
-    {
-        set_time_limit(900);
-        $date = Carbon::now();
-        DB::table('hats')->truncate();
-        for ($store = 1; $store < 21; $store++) {
-            for ($i = 1; $i < 2000; $i++) {
-                Hats::updateOrCreate(
-                    [
-                        'store' => $store,
-                        'date' => $date->copy()->subDays($i)->format('Y-m-d')
-                    ],
-                    [
-                        'cashbox' => rand(1000, 6000),
-                        'cashbox_ret' => rand(1000, 5000),
-                        'income' => rand(100, 1000),
-                        'profit' => rand(-500, 500),
-                        'income_piece' => rand(1, 100),
-                        'rent' => rand(100, 500),
-                        'salary' => rand(100, 200),
-                        'other_costs' => rand(100, 300),
-                        'distributed_costs' => rand(100, 300),
-                        'discounts' => rand(50, 500),
-                        'items_sold' => rand(10, 30),
-                        'items_sold_ret' => rand(0, 20),
-                        'items_returned' => rand(0, 20),
-                        'unique_sku' => rand(10, 200),
-                        'area' => rand(3, 200),
-                        'dollar_rate' => rand(2700, 2800) / 100,
-                        'SUPERINCOME' => rand(-100, 1000),
-                    ]
-                );
-            }
-        }
-    }
-
 
 
     public function get()
@@ -100,6 +64,7 @@ class HatsController extends BaseHatsController
                 $arr[$key][] = $val;
             }
         }
+        $this->stores = $arr['store'];
         return $arr;
     }
 
@@ -109,7 +74,7 @@ class HatsController extends BaseHatsController
         $date_end = DB::table('hats')->max('date');
         $monthes = Carbon::parse($date_end)->diffInMonths($date_start);
         $date = Carbon::now();
-//        dd($monthes);
+//        dd($date_start);
 
         for ($i = 1; $i <= $monthes; $i++) {
             $end = $date->copy()->subMonths($i)->lastOfMonth()->format('Y-m-d');
@@ -136,7 +101,7 @@ class HatsController extends BaseHatsController
         $end = $date->copy()->format('Y-m-d');
         $start = $date->copy()->firstOfMonth()->format('Y-m-d');
         $period = '_1';
-
+        $stores = implode(', ', $this->stores);
         // cashbox_ret_piece  --доля розничеой кассы к кассе
         // income_piece_percent  --доля дохода от себестоимости
 
@@ -149,7 +114,8 @@ class HatsController extends BaseHatsController
                                     Sum(SUPERINCOME) as SUPERINCOME' . $period . ',
                                     Sum(items_sold) as items_sold' . $period . ',
                                     (Sum(income_piece) / COUNT(income_piece)) as income_piece_percent' . $period . '
-                                FROM `hats` WHERE date BETWEEN \'' . $start . '\' and \'' . $end . '\'
+                                FROM `hats` WHERE date BETWEEN \'' . $start . '\' and \'' . $end . '\' and
+                                store in (' . $stores . ')
                                 GROUP BY store order by store');
         foreach ($data as &$d) {
             $d = (array)$d;
@@ -168,10 +134,10 @@ class HatsController extends BaseHatsController
         $end = $date->copy()->format('Y-m-d');
         $start = $date->copy()->subMonth()->format('Y-m-d');
         $period = '_М';
-
+        $stores = implode(', ', $this->stores);
         // SUPERINCOME_piece  -- доля супердохода от дохода
 
-        $data =DB::select('SELECT
+        $data = DB::select('SELECT
                                     Sum(cashbox / dollar_rate) as cashbox_$' . $period . ',
                                     Sum(income / dollar_rate) as income_$' . $period . ',
                                     Sum(profit / dollar_rate) as profit_$' . $period . ',
@@ -179,7 +145,8 @@ class HatsController extends BaseHatsController
                                     Sum(SUPERINCOME) as SUPERINCOME' . $period . ',
                                     (Sum(SUPERINCOME) / Sum(income) *100) as SUPERINCOME_piece' . $period . ',
                                     Sum(items_returned) as items_returned' . $period . '
-                                FROM `hats` WHERE date BETWEEN \'' . $start . '\' and \'' . $end . '\'
+                                FROM `hats` WHERE date BETWEEN \'' . $start . '\' and \'' . $end . '\'and
+                                store in (' . $stores . ')
                                 GROUP BY store order by store');
 
         foreach ($data as &$d) {
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,65 +1,65 @@
-@extends('layouts.main')
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+          integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <title>Отчеты</title>
+</head>
+<body>
+<div class="container" style="width: 400px;">
+    <div id="login-form">
+        <div class="row">
+            <div class="col">
 
-
-@section('content')
-    <div class="container">
-
-        <div id="login-form">
-            <div class="row">
-                <div class="col">
-
-                    <form action="{{ route('login.login') }}" method="POST">
-                        <input type="hidden" name="_method" value="POST">
-                        @csrf
-                        @if($errors->any())
-                            <div class="m-auto">
-                                @foreach ($errors->all() as $error)
-                                    <p style="color:red; text-align: center">{{ $error }}</p>
-                                @endforeach
-                            </div>
-                        @endif
-                        <div class="form-row">
-                            <div class="form-group col-12">
-                                <label for="login">Логин</label>
-                                <input type="text" class="form-control" id="login" name="login">
-                            </div>
-                            <div class="form-group col-12">
-                                <label for="password">Пароль</label>
-                                <input type="password" class="form-control" id="password" name="password">
-                            </div>
-
+                <form action="{{ route('login.login') }}" method="POST">
+                    <input type="hidden" name="_method" value="POST">
+                    @csrf
+                    @if($errors->any())
+                        <div class="m-auto">
+                            @foreach ($errors->all() as $error)
+                                <p style="color:red; text-align: center">{{ $error }}</p>
+                            @endforeach
+                        </div>
+                    @endif
+                    <div class="form-row">
+                        <div class="form-group col-12">
+                            <label for="login">Логин</label>
+                            <input type="text" class="form-control" id="login" name="login">
+                        </div>
+                        <div class="form-group col-12">
+                            <label for="password">Пароль</label>
+                            <input type="password" class="form-control" id="password" name="password">
                         </div>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                    </div>
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Запомнить') }}
-                                    </label>
-                                </div>
+                    <div class="form-group row">
+                        <div class="col-md-6 offset-md-4">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                <label class="form-check-label" for="remember">
+                                    {{ __('Запомнить') }}
+                                </label>
                             </div>
                         </div>
+                    </div>
 
 
-                        <div class="form-row">
-                            <div class="col-12">
-                                <button class="btn btn-primary btn-block">Войти</button>
-                            </div>
+                    <div class="form-row">
+                        <div class="col-12">
+                            <button class="btn btn-primary btn-block">Войти</button>
                         </div>
+                    </div>
 
-                    </form>
+                </form>
 
-                </div>
             </div>
         </div>
-
     </div>
-@endsection
-
-@section('scripts')
-
-@endsection
+</div>
+</body>
+</html>
 
 
